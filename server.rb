77438709562a7d829d7392151d53f75c802a8978
Pat Kelly <pkellz6@me.com>
2015-11-04 14:08:04 -0500

    -

--- server.rb
+++ server.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
 
-
+# post '/' do
+#   "this is posting\n"
+# end
 #
 get '/' do
-  File.read(File.join('public', 'portfolio.html'))
+  File.read(File.join('public', 'index.html'))
 end
+
+
+
+# url = "http://finance.yahoo.com/q?s=AAPL"
+#
+# response = HTTParty.get url
+#
+# dom = Nokogiri::HTML(response.body)
+#
+# stock_price = dom.xpath('//*[@id="yfs_l84_aapl"]').first.content
+#
+# get '/' do
+# "Apple's stock is at #{stock_price}"
+# end
